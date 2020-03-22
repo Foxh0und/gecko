32d4313af14dec93df05bc179a6511dff2ef7bac
Matthew Aylward <matthewtyleraylward@hotmail.com>
2020-03-22 21:02:24 +1100

    removed API Keys

--- gecko/main.py
+++ gecko/main.py
@@ -1,7 +1,7 @@
 from trader import Trader
 
-API_KEY = "c67aefb2-1894-4674-8c2b-3399ad612e07"
-SECRET_KEY = "lVHKgk5pVbpLHeZxiBdtlp0wwq/L/uk+n4nmWXfZz1BSHQdmFT9xWST4Us297XeuxV7YPmTQh2ZT2m3r9aDxjA=="
+API_KEY = ""
+SECRET_KEY = ""
 
 Trader = Trader( API_KEY, SECRET_KEY )
 print( Trader.getAllOrders() )
