d1d6de9ed49557ae63ada096b6c4b1042ba02b61
Matthew Aylward <matthewtyleraylward@hotmail.com>
2020-03-22 21:00:55 +1100

    Added API calls

--- gecko/main.py
+++ gecko/main.py
@@ -1,7 +1,9 @@
 from trader import Trader
 
-API_KEY = ""
-SECRET_KEY = ""
+API_KEY = "c67aefb2-1894-4674-8c2b-3399ad612e07"
+SECRET_KEY = "lVHKgk5pVbpLHeZxiBdtlp0wwq/L/uk+n4nmWXfZz1BSHQdmFT9xWST4Us297XeuxV7YPmTQh2ZT2m3r9aDxjA=="
 
-lTrader = Trader( API_KEY, SECRET_KEY )
-print( lTrader.getAllOrders() )
+Trader = Trader( API_KEY, SECRET_KEY )
+print( Trader.getAllOrders() )
+print( Trader.get_account_balance() )
+print( Trader.get_BTC_ticketer() )
